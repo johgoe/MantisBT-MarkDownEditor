b91029dec38d0a48cf4e269076210afc41d293d4
Johannes Göhr <johannes.goehr@mobilexag.de>
2017-09-13 17:48:55 +0200

    Show only member of projects (+1 squashed commits)
    Squashed commits:
    
    [7beefaa] Show only member of projects

--- pages/search.php
+++ pages/search.php
@@ -2,8 +2,9 @@
 access_ensure_global_level( REPORTER );
 
 $f_term	  = gpc_get_string( 'term' );
+$t_project_id = helper_get_current_project( );
 
-$t_query = "SELECT username FROM {user} WHERE (username LIKE '%".$f_term. "%' OR realname LIKE '%".$f_term."%') AND enabled=1";
+$t_query = "SELECT username FROM {user} WHERE (username LIKE '%".$f_term. "%' OR realname LIKE '%".$f_term."%') AND enabled=1 AND EXISTS (SELECT * FROM {project_user_list} WHERE project_id = " .strval($t_project_id). " AND {user}.id = user_id)";
 $t_result = db_query( $t_query );
 
 $t_users = array();
